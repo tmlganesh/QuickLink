5163682a97708b3f10847c8440fae4c63618fa8b
tmlganesh <tmlganesh24@gmail.com>
2025-09-02 13:12:43 +0530

    Update .gitignore to exclude .md files, .bat files, and test files

--- run-demo.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Running URL Shortener Demo...
-echo.
-echo Make sure the server is running in another terminal:
-echo   start-server.bat
-echo.
-timeout /t 3 /nobreak >nul
-go run -tags demo demo.go
--- run-tests.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Running URL Shortener Tests...
-echo.
-go test -v
-echo.
-echo Running tests with race detection...
-go test -v -race
-echo.
-echo Tests completed!
